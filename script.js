5473ada62f15c65b86031ee7d4f54ebd623467a2
bikashpaulbp <bikashpaulbp92@gmail.com>
2021-03-14 13:13:04 +0600

    done

--- script.js
+++ script.js
@@ -1,18 +1,66 @@
-const image = [{img:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/The_cricket_legend_Sachin_Tendulkar_at_the_Oval_Maidan_in_Mumbai_During_the_Duke_and_Duchess_of_Cambridge_Visit%2826271019082%29.jpg/390px-The_cricket_legend_Sachin_Tendulkar_at_the_Oval_Maidan_in_Mumbai_During_the_Duke_and_Duchess_of_Cambridge_Visit%2826271019082%29.jpg"
-}, {img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Shakib_Al_Hasan_%284%29_%28cropped%29.jpg/390px-Shakib_Al_Hasan_%284%29_%28cropped%29.jpg"
-} ]
+const imageVertical = [{img:"./img/Photos/imgv1.jpg"
+},{img: "./img/Photos/imgv2.jpg"
+},{img: "./img/Photos/imgv3.jpg"
+},{img: "./img/Photos/imgv4.jpg"
+},{img: "./img/Photos/imgv5.jpg"
+},{img: "./img/Photos/imgv6.jpg"
+},{img: "./img/Photos/imgv7.jpg"
+},{img: "./img/Photos/imgv8.jpg"
+},{img: "./img/Photos/imgv9.jpg"
+},{img: "./img/Photos/imgv10.jpg"
+},{img: "./img/Photos/imgv11.jpg"
+},{img: "./img/Photos/imgv12.jpg"
+},{img: "./img/Photos/imgv13.jpg"
+},{img: "./img/Photos/imgv14.jpg"
+},{img: "./img/Photos/imgv15.jpg"
+},{img: "./img/Photos/imgv16.jpg"
+} ];
+
+
+const image = [{img:"./img/Photos/img1.jpg"
+},{img:"./img/Photos/img2.jpg"
+},{img:"./img/Photos/img3.jpg"
+},{img:"./img/Photos/img4.jpg"
+},{img:"./img/Photos/img5.jpg"
+},{img:"./img/Photos/img6.jpg"
+}];
+
+
+document.getElementById("photo").addEventListener("click",function (){
+
+    
+    for (let i = 0; i < image.length; i++) {
+        const element = imageVertical[i];
+        const packageDiv = document.getElementById("package-div")
+        packageDiv.classList.add("display-package-div")
+        const imageDiv = document.getElementById("image-div")
+       const newDiv = document.createElement("div")
+       newDiv.className = "display-image col-md-4"
+       newDiv.innerHTML= `
+       <img src="${element.img}" alt="">
+       `
+       
+       imageDiv.appendChild(newDiv);
+       
+    }
+})
+
 document.getElementById("photo").addEventListener("click",function (){
 
     
     for (let i = 0; i < image.length; i++) {
         const element = image[i];
+        const packageDiv = document.getElementById("package-div")
+        packageDiv.classList.add("display-package-div")
         const imageDiv = document.getElementById("image-div")
        const newDiv = document.createElement("div")
-       newDiv.innerHTML= `<img src="${element.img}" alt="">`
+       newDiv.className = "display-h-image col-md-4"
+       newDiv.innerHTML= `
+       <img src="${element.img}" alt="">
+       `
        imageDiv.appendChild(newDiv)
-       console.log(element.img)
+       
     }
 })
-
     
 
